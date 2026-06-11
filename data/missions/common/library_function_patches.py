996aac51c185fecd6bffd4160845448e318e16b7
Quaris <quaris314@gmail.com>
2026-06-11 01:38:03 -0500

    Fix vanilla issues 658, 659, 660, and 661 - Fix many upgrades gui bugs

--- data/missions/common/library_function_patches.py
+++ data/missions/common/library_function_patches.py
@@ -1,10 +1,239 @@
 import re
 
+from sbs_utils.gui import get_client_aspect_ratio
 from sbs_utils.helpers import FakeEvent, FrameContext, FrameContextOverride
+from sbs_utils.mast.label import label
+from sbs_utils.pages.layout.blank import Blank
+from sbs_utils.pages.layout.button import Button
 from sbs_utils.pages.layout.column import Column
-from sbs_utils.procedural.execution import get_variable
+from sbs_utils.pages.layout.layout import Layout
+from sbs_utils.pages.layout.row import Row
+from sbs_utils.procedural.execution import AWAIT, END, get_variable, gui_task_jump, task_schedule
+from sbs_utils.procedural.gui import ButtonPromise
+from sbs_utils.procedural.gui.navigation import gui_reroute_client, gui_reroute_server
 from sbs_utils.procedural.gui.property_listbox import _gui_properties_items
 from sbs_utils.procedural.style import apply_control_styles
+from sbs_utils.procedural.timers import delay_app
+
+# ----- switching GUIs -----
+
+# The relationship between (re)drawing a GUI and what task/code the
+# redraw is happening on and was triggered by seems VERY fragile.
+# https://github.com/artemis-sbs/LegendaryMissions/issues/579
+# https://github.com/artemis-sbs/LegendaryMissions/issues/590
+# https://github.com/orgs/artemis-sbs/discussions/628
+# https://github.com/artemis-sbs/LegendaryMissions/issues/634
+# https://github.com/artemis-sbs/LegendaryMissions/issues/635
+# So centralize the process of switching between GUIs to this function,
+# so that (hopefully) I don't have to keep doing shotgun surgery
+# whenever a new gui-switching bug is discovered.
+
+def gui_switch_to(main_gui_label, client_id=None, delay_reroute_workaround=False):
+    """
+    Switches a client from its current gui to a completely new gui.
+    (For example, switch from the console selection screen to the helm screen.)
+    
+    Args:
+        main_gui_label (string | label object): The main entry point for code
+            that creates a gui.
+        client_id (int | None): Default get_variable("client_id"). The client_id
+            whose gui will be switched to main_gui_label.
+        delay_reroute_workaround (bool | None): Default False. Adds a small delay
+            before switching guis in order to work around this issue
+            https://github.com/artemis-sbs/LegendaryMissions/issues/634
+    """
+    if client_id is None:
+        client_id = get_variable("client_id")
+    
+    if delay_reroute_workaround:
+        task_schedule(_gui_switch_to_delay_reroute_workaround, data={"MAIN_GUI_LABEL": main_gui_label, "CLIENT_ID": client_id})
+    else:
+        reroute(client_id, main_gui_label)
+
+@label()
+def _gui_switch_to_delay_reroute_workaround():
+    client_id = get_variable("CLIENT_ID")
+    main_gui_label = get_variable("MAIN_GUI_LABEL")
+    yield AWAIT(delay_app(0.01))
+    reroute(client_id, main_gui_label)
+    yield END()
+
+def ensure_on_gui_task(main_gui_label):
+    """
+    Checks whether the current task is the correct task on which to draw a gui.
+    If it is, returns True and doesn't do anything else (since everything is fine).
+    But if it isn't, returns False and makes the correct gui task switch to the
+    passed label. (In this case, code calling this function should probably ->END.)
+    
+    This function is primarily intended to be called at the beginning of gui code like so:
+        ==== main_gui_label ====
+            
+            if not ensure_on_gui_task(main_gui_label):
+                ->END
+            
+            # insert gui_section(), gui_text(), etc. calls here
+            
+            await gui()
+    
+    Args:
+        main_gui_label (str): The label to jump the correct gui task to (if necessary).
+    
+    Returns:
+        True if this function was called from the correct task on which to create a gui;
+            otherwise False.
+    """
+    if FrameContext.task == FrameContext.page.gui_task:
+        return True
+    else:
+        gui_switch_to(main_gui_label)
+        return False
+
+# sbs_utils/procedural/gui/gui.py function gui()
+# It should assert that it should run on FrameContext.page.gui_task, not FrameContext.client_task
+# https://github.com/orgs/artemis-sbs/discussions/628#discussioncomment-16919456
+def gui_patched(buttons=None, timeout=None):
+    """present the gui that has been queued up
+    
+    Args:
+        buttons (dict, optional): _description_. Defaults to None.
+        timeout (promise, optional): A promise that ends the gui. Typically a timeout. Defaults to None.
+    
+    Returns:
+        Promise: The promise for the gui, promise is done when a button is selected
+    """
+    page = FrameContext.page
+    task = FrameContext.task
+    
+    #gui_task = FrameContext.client_task # bad
+    gui_task = FrameContext.page.gui_task # patch
+    
+    ret = GuiPromise(page, timeout)
+    if buttons is not None:
+        for k in buttons:
+            ret.buttons.append(Button(k, label=buttons[k],loc=0))
+    
+    if task != gui_task:
+        print("await gui() was not called in gui's main task. Consider using gui_task_jump.")
+    else:
+        page.swap_gui_promise(ret)
+    return ret
+
+# sbs_utils/procedural/gui/gui.py class GuiPromise
+# Not edited from its original form.
+# Copied here only because it apparently can't be imported from sbs_utils.
+class GuiPromise(ButtonPromise):
+    button_height_px = 40
+
+    def __init__(self, page, timeout=None) -> None:
+        path = page.get_path()
+        super().__init__(path, page.gui_task, timeout)
+
+        self.page = page
+        self.button_layout = None
+
+    def initial_poll(self):
+        if self._initial_poll:
+            return
+        
+        super().initial_poll()
+        self.show_buttons()
+        self.page.set_button_layout(self.button_layout, self)
+
+    #
+    # This
+    #
+    def show_buttons(self):
+        if self.task is None: # pylint: disable=access-member-before-definition
+            self.task = self.page.gui_task # pylint: disable=attribute-defined-outside-init
+            # First run could have no gui_task
+            if self.task is None:
+                self.task = FrameContext.task # pylint: disable=attribute-defined-outside-init
+        task = self.task
+        aspect_ratio = get_client_aspect_ratio(task.main.page.client_id)
+
+        #
+        # Create button Row
+        #
+        top = ((aspect_ratio.y - GuiPromise.button_height_px)/aspect_ratio.y)*100
+
+        button_layout = Layout(None, None, 0,top,100,100)
+        button_layout.tag = task.main.page.get_tag()
+
+        active = 0
+        index = 0
+        layout_row: Row
+        layout_row = Row()
+        layout_row.tag = task.main.page.get_tag()
+
+        buttons = self.get_expanded_buttons()
+        
+        if len(buttons) == 0:
+            return
+        
+        
+        for button in buttons:
+            match button.__class__.__name__:
+                case "Button":
+                    value = True
+                    #button.end_await_node = node.end_await_node
+                    if button.code is not None:
+                        value = task.eval_code(button.code)
+                    if value and button.should_present(0):#task.main.client_id):
+                        runtime_node = ChoiceButtonRuntimeNode(self, button, task.main.page.get_tag())
+                        #runtime_node.enter(mast, task, button)
+                        msg = task.format_string(button.message)
+                        layout_button = Button(runtime_node.tag, msg)
+                        button.layout_item = layout_button
+                        layout_row.add(layout_button)
+
+                        apply_control_styles(".choice", None, layout_button, task)
+                        
+                        # After style could change tag
+                        task.main.page.add_tag(layout_button, runtime_node)
+                        active += 1
+                case "Separator":
+                    # Handle face expression
+                    layout_row.add(Blank())
+            index+=1
+        
+        if active>0:
+            button_layout.add(layout_row)
+            self.button_layout = button_layout
+            #task.main.page.set_button_layout(button_layout)
+        else:
+            self.button_layout = None
+            #task.main.page.set_button_layout(None)
+
+        self.active_buttons = active # pylint: disable=attribute-defined-outside-init
+        self.buttons = buttons # pylint: disable=attribute-defined-outside-init
+        self.button = None # pylint: disable=attribute-defined-outside-init
+
+# sbs_utils/procedural/gui/gui.py class ChoiceButtonRuntimeNode
+# Not edited from its original form.
+# Copied here only because it apparently can't be imported from sbs_utils.
+class ChoiceButtonRuntimeNode: # pylint: disable=too-few-public-methods
+    def __init__(self, promise, button, tag):
+        self.promise = promise
+        self.button = button
+        self.tag = tag
+    
+    def on_message(self, event):
+        #
+        # The 'right' page already filtered
+        # event to know it is for this client
+        #
+        if event.sub_tag == self.tag:
+            self.promise.press_button(self.button)
+
+# ----- reroute -----
+
+def reroute(client_id, label_for_both_or_server, label_for_client=None):
+    if client_id == 0:
+        gui_reroute_server(label_for_both_or_server)
+    else:
+        if label_for_client is None:
+            label_for_client = label_for_both_or_server
+        gui_reroute_client(client_id, label_for_client)
 
 # ----- text input -----
 
